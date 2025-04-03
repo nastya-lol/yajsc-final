cf5dc1c7cec628be263d38e57d82b03e3f8d2aba
Nastya Lol <anastasiia1297@gmail.com>
2025-04-03 21:33:49 +0300

    small changes

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -5,7 +5,7 @@ test("Verify login with valid credentials", async ({ page }) => {
   await page.locator('[data-test="email"]').fill(process.env.USER_EMAIL);
   await page.locator('[data-test="password"]').fill(process.env.USER_PASSWORD);
   await page.locator('[data-test="login-submit"]').click();
-  await expect(page).toHaveURL(`${process.env.WEB_URL}/account`);
+  await expect(page).toHaveURL('/account');
   await expect(page.locator('[data-test="page-title"]')).toContainText("My account");
   await expect(page.locator('[data-test="nav-menu"]')).toContainText(process.env.USER_NAME);
 });
@@ -14,8 +14,7 @@ test("Verify login with valid credentials", async ({ page }) => {
 test("Verify user can view product details", async ({ page }) => {
   await page.goto('/')
   await page.getByText('Combination Pliers').click();
-  const currentURL = page.url();
-  expect(currentURL).toContain(`${process.env.WEB_URL}/product`);
+  expect(page.url()).toContain('/product');
   await expect(page.locator('[data-test="product-name"]')).toContainText('Combination Pliers');
   await expect(page.locator('[data-test="unit-price"]')).toContainText('14.15');
   await expect(page.locator('[data-test="add-to-cart"]')).toBeVisible();
@@ -34,7 +33,7 @@ test("Verify user can add product to cart", async ({ page }) => {
   await page.waitForSelector('alert', { state: 'detached', timeout: 8000 });
   await expect(page.locator('[data-test="cart-quantity"]')).toContainText('1');
   await page.locator('[data-test="nav-cart"]').click();
-  await expect(page).toHaveURL(`${process.env.WEB_URL}/checkout`);
+  await expect(page).toHaveURL('/checkout');
   await expect(page.locator('tbody').getByRole('row')).toHaveCount(1);
   await expect(page.locator('[data-test="product-title"]')).toContainText('Slip Joint Pliers');
   await expect(page.locator('[data-test="proceed-1"]')).toBeVisible();
